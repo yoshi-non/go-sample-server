94da47c33f3b7a9ba77f727fad4877dc28ffcdbf
yoshi-non <himekawa.yoshino44@gmail.com>
2023-03-09 09:34:31 +0900

    curlコマンドでリクエストするたびにcountを上げる

---
+++ README.md
@@ -0,0 +1,5 @@
+```bash
+go run main.go
+# 別のターミナルで実行
+curl localhost:8080
+```
--- main.go
+++ main.go
@@ -5,11 +5,19 @@ import (
 	"net/http"
 )
 
+var count int
+
 func main() {
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/get", countHandler)
 	http.ListenAndServe(":8080", nil)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "hello")
+	count++
+	fmt.Fprintln(w, count)
+}
+
+func countHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, count)
 }
